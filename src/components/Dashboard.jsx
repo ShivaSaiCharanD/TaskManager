157b891f8e550252cfb1359b2610607ade1366d1
Shiva Sai Charan Dodda <shivasaicharandodda@gmail.com>
2024-08-06 23:33:44 +0530

    navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Dashboard.css';
-import task from '../../backend/models/task';
+
 
 export default function Dashboard() {
     const [tasks, setTasks] = useState([]);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,190 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './navbar.css'; 
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import "./navbar.css";
+import { useEffect, useState } from "react";
+import SuprSendInbox from "@suprsend/react-inbox";
+import axios from "axios";
 
-export default function Navbar() {
-    const navigate = useNavigate();
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsAuthenticated(true);
-        } else {
-            setIsAuthenticated(false);
+const Navbar = () => {
+  const [user, setUser] = useState();
+  const [login, setLogin] = useState(false);
+  const [subscriberId, setSubscriberId] = useState();
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        setLogin(false);
+        return;
+      }
+      try {
+        try {
+          const response = await axios.post(
+            "https://tdlback.vercel.app/api/users/check",
+            { token }
+          );
+          if (response.status === 200) {
+            setLogin(true);
+          }
+          setUser(response.data.name);
+        } catch (err) {
+          setLogin(false);
         }
-    }, []);
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        setIsAuthenticated(false);
-        navigate('/');
+      } catch (err) {
+        setLogin(false);
+      }
     };
 
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Task Manager</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ms-auto">
-                        {!isAuthenticated ? (
-                            <>
-                                <li className="nav-item">
-                                    <Link to='/login' className="btn btn-outline-success mx-2">
-                                        Login
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to='/register' className="btn btn-outline-success">
-                                        Register
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
-                            <li className="nav-item">
-                                <button className="btn btn-outline-success" onClick={handleLogout}>
-                                    Logout
-                                </button>
-                            </li>
-                        )}
-                    </ul>
-                </div>
+    checkLoginStatus();
+  }, [location]);
+
+  useEffect(() => {
+    const generateSubscriberId = async () => {
+      if (!login) return;
+      try {
+        let username = localStorage.getItem("username");
+        const subscriberResponse = await axios.post(
+          "https://tdlback.vercel.app/api/subsid/subsId_generate",
+          { distinct_id: username }
+        );
+
+        setSubscriberId(subscriberResponse.data);
+      } catch (err) {
+        return;
+      }
+    };
+
+    generateSubscriberId();
+  }, [login, user]);
+
+  const handlelogout = async (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setLogin(false);
+    navigate("/");
+  };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-light shadow rounded m-3"
+        style={{ width: "90%", maxWidth: "1200px" }}
+      >
+        <div className="container-fluid">
+          {!login && (
+            <Link className="navbar-brand d-flex align-items-center" to="/">
+              <img
+                src="./task-list.svg"
+                alt="logo"
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  padding: "0.8px",
+                }}
+              />
+              <span className="fs-5 text-dark fw-semibold ms-2">
+                Task Manager
+              </span>
+            </Link>
+          )}
+          {login && (
+            <Link
+              className="navbar-brand d-flex align-items-center"
+              to="/userhome"
+            >
+              <img
+                src="./task-list.svg"
+                alt="logo"
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  padding: "0.8px",
+                }}
+              />
+              <span className="fs-5 text-dark fw-semibold ms-2">Home</span>
+            </Link>
+          )}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {!login && (
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link text-dark" to="/login">
+                    Login
+                  </Link>
+                </li>
+              </ul>
             </div>
-        </nav>
-    );
-}
+          )}
+          {login && (
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-dark fw-bold"
+                    to="/userprofile"
+                  >
+                    {user}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-danger fw-bold"
+                    onClick={handlelogout}
+                  >
+                    Logout
+                  </Link>
+                </li>
+                <SuprSendInbox
+                  theme={{
+                    badge: {
+                      backgroundColor: "pink",
+                      color: "black",
+                      margin: "0px",
+                    },
+                    bell: { color: "blue" },
+                    header: {
+                      container: { backgroundColor: "#0099ff" },
+                      headertext: { color: "black" },
+                      markAllReadText: { color: "black", fontWeight: "bold" },
+                    },
+                    notification: {
+                      actions: {
+                        container: { hoverBackgroundColor: "#349beb" },
+                      },
+                      expiresText: { color: "red" },
+                      actionsMenuIcon: { color: "blue" },
+                    },
+                  }}
+                  themeType="light / dark"
+                  workspaceKey={process.env.WorkspaceKey}
+                  
+                  subscriberId={subscriberId}
+                  distinctId={localStorage.getItem("username")}
+                />
+              </ul>
+            </div>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
